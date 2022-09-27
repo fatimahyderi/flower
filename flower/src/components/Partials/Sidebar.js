cc41e438c008192c180806d93e83ebdaa73275d8
fatimahyderi <fatimakhurram84@gmail.com>
2022-09-28 00:06:49 +0500

    heroku

--- flower/src/components/Partials/Sidebar.js
+++ flower/src/components/Partials/Sidebar.js
@@ -66,7 +66,7 @@ function Sidebar() {
                                     {categories && categories.map((data, key) => {
                                     return (
                                         <>
-                                        <li value={data.name} >{data.name}</li>
+                                        <li>{data.name}</li>
                                         </>
                                     )
                                 })}
